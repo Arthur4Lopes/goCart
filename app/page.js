a92c0844062c488b72f25ecc74eb828e90efee04
Arthur Birck Lopes <arthur.bircklopes@edu.sait.ca>
2024-05-16 11:27:41 -0600

    now its correct

--- app/page.js
+++ app/page.js
@@ -1,15 +1,16 @@
 import Image from "next/image";
 import Header from "@/components/header";
+import Footer from "@/components/footer";
 
 export default function Home() {
   return (
-    <main className="min-h-screen bg-teal-100 w-full flex-col justify-center items-center py-2">
-      <div className="text-gray-700 w-4/5 bg-red-200">
-        <Header/>
+    <main className="min-h-screen bg-teal-100 w-full flex flex-col justify-center items-center">
+      <Header />
+      <div className="bg-gray-50 w-4/5 flex flex-col justify-center items-center py-2 min-h-screen">
+        Hello Again. Welcome to wedbev class. I am changing a bunch of things to
+        demonstrate ci/cd. Continous integration, continous deployment.
       </div>
-      <div className="bg-blue-200 w-4/5">
-        Hello, welcome to dev class
-        </div>
+      <Footer />
     </main>
   );
 }
